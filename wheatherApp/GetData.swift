b8400e4ca1baaaaa2d8ab163207c784083c5bcb5
Gaurav k <gauravk@in-gaurav.local>
2016-05-04 12:34:14 +0530

    renamed folders and added on button on storuy board

---
+++ wheatherApp/GetData.swift
@@ -0,0 +1,13 @@
+//
+//  GetData.swift
+//  wheatherApp
+//
+//  Created by Gaurav k on 5/4/16.
+//  Copyright © 2016 Gaurav k. All rights reserved.
+//
+
+import UIKit
+
+class GetData: NSObject {
+
+}
